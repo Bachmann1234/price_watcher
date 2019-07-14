9c6e43e232bb99236454a379c38357adf8bfbbbf
Matt Bachmann <bachmann.matt@gmail.com>
2019-07-14 16:06:06 -0400

    Got the basic thing working now. Needs more tests and some docs. Then packaging

--- price_watcher/cli.py
+++ price_watcher/cli.py
@@ -20,10 +20,11 @@ def write_result(history_file, prices):
 @click.command()
 @click.argument("product_id", type=click.STRING)
 @click.argument("target_price", type=click.FLOAT)
+@click.argument("phone_number", type=click.STRING)
 @click.argument(
     "history_file", type=click.Path(dir_okay=False, writable=True), required=False
 )
-def check_product(product_id, target_price, history_file):
+def check_product(product_id, target_price, phone_number, history_file):
     product_info = get_product_info(product_id)
     minimum_price = min(product_info.prices)
     print("Min: {}".format(minimum_price))
@@ -32,7 +33,8 @@ def check_product(product_id, target_price, history_file):
         write_result(history_file, product_info.prices)
     if minimum_price <= target_price:
         send_text(
-            "Found {} for {} dollars! {}".format(
+            "Found {} for ${}! {}".format(
                 product_info.name, minimum_price, get_url(product_id)
-            )
+            ),
+            phone_number
         )
--- price_watcher/notifications.py
+++ price_watcher/notifications.py
@@ -1,2 +1,15 @@
-def send_text(text):
-    print(text)
+import os
+
+from twilio.rest import Client as TwilioClient
+
+
+def send_text(text, recipient_phone):
+    # I am assuming a script that runs an exists in a cron job. If this becomes
+    # Long living in some future this should be re thought
+    TwilioClient(
+        os.environ["PRICE_WATCHER_SID"], os.environ["PRICE_WATCHER_AUTH"]
+    ).messages.create(
+        body=text,
+        from_=os.environ["PRICE_WATCHER_PHONE_NUMBER"],
+        to=recipient_phone,
+    )
